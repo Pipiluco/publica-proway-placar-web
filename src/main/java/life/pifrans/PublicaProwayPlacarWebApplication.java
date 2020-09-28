55885fa48a21eef2af1c0b88d5a13964efbb628c
Lucas Francisco <lucasf.silva@live.com>
2020-09-28 20:41:19 -0300

    Criação e configuração do projeto web

---
+++ src/main/java/life/pifrans/PublicaProwayPlacarWebApplication.java
@@ -0,0 +1,13 @@
+package life.pifrans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicaProwayPlacarWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicaProwayPlacarWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/life/pifrans/PublicaProwayPlacarWebApplicationTests.java
@@ -0,0 +1,13 @@
+package life.pifrans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicaProwayPlacarWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
