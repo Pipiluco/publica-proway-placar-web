0b0e63f15dd3509a2dfcc69bf58550f822393a47
Lucas Francisco <lucasf.silva@live.com>
2020-10-01 14:01:00 -0300

    Implementação da página seasons.xhtml

--- src/main/java/life/pifrans/beans/SeasonBean.java
+++ src/main/java/life/pifrans/beans/SeasonBean.java
@@ -1,33 +1,43 @@
 package life.pifrans.beans;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.enterprise.context.RequestScoped;
+import javax.annotation.PostConstruct;
 import javax.inject.Named;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 
 import life.pifrans.controllers.GenericController;
 import life.pifrans.models.Season;
 
 @Named
-@RequestScoped
-public class SeasonBean {
+@Scope(value = "view")
+public class SeasonBean implements Serializable {
+	private static final long serialVersionUID = 1L;
+
 	@Autowired
 	private GenericController<Season> controller;
 
 	@Autowired
 	private Season season;
-	
-	private static final String SUB_PATH = "seasons";
 
-	public void findAll() {
-		List<Season> list = new ArrayList<>();
-		list = controller.listAll(Season[].class, SUB_PATH);
-		for (Season season : list) {
-			System.out.println(season.getMaximumRecord() + " - " + season.getMinimumRecord());
-		}
+	private List<Season> seasons;
+
+	private static final String SUB_PATH = "seasons";
+	private static final String PAGE_SEASONS = "/private/seasons.jsf";
+
+	@PostConstruct
+	public void init() {
+		seasons = controller.listAll(Season[].class, SUB_PATH);
+	}
+
+	public List<Season> findAll() {
+		seasons = new ArrayList<>();
+		seasons = controller.listAll(Season[].class, SUB_PATH);
+		return seasons;
 	}
 
 	public void find() {
@@ -35,16 +45,31 @@ public class SeasonBean {
 		System.out.println(season.getMaximumRecord() + " - " + season.getMinimumRecord());
 	}
 
-	public void save() {
+	public void renew() {
+		season = new Season();
+	}
+
+	public String save() {
+		season.setMinimumRecord(0);
+		season.setMaximumRecord(0);
 		controller.save(season, SUB_PATH);
+		return PAGE_SEASONS;
 	}
 
-	public void update() {
+	public String update() {
 		controller.update(season, season.getId(), SUB_PATH);
+		return PAGE_SEASONS;
 	}
 
-	public void delete() {
+	public String delete(Season season) {
 		controller.delete(season.getId(), SUB_PATH);
+		return PAGE_SEASONS;
+	}
+
+	public String delete() {
+		controller.delete(season.getId(), SUB_PATH);
+		season = new Season();
+		return PAGE_SEASONS;
 	}
 
 	public Season getSeason() {
@@ -54,4 +79,13 @@ public class SeasonBean {
 	public void setSeason(Season season) {
 		this.season = season;
 	}
+
+	public List<Season> getSeasons() {
+		return seasons;
+	}
+
+	public void setSeasons(List<Season> seasons) {
+		this.seasons = seasons;
+	}
+
 }
