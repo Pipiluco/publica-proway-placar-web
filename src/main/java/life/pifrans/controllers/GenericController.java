3fd7e46202bcad26c9f02e8539bc9cf7a89c706b
pifrans <lucasf.silva@live.com>
2020-10-03 21:45:08 -0300

    Tratação de erro no método save() de GenericController

--- src/main/java/life/pifrans/controllers/GenericController.java
+++ src/main/java/life/pifrans/controllers/GenericController.java
@@ -3,14 +3,24 @@ package life.pifrans.controllers;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+
+import life.pifrans.exceptions.StandardError;
 
 @Controller
 public class GenericController<T> {
 	@Autowired
 	private RestTemplate restTemplate;
+
+	private JSONObject json;
+	private StandardError standardError;
 
 	public List<T> listAll(Class<T[]> model, String subPath) {
 		String url = "http://localhost/" + subPath;
@@ -25,8 +35,14 @@ public class GenericController<T> {
 	}
 
 	public void save(T object, String subPath) {
-		String url = "http://localhost/" + subPath;
-		restTemplate.postForObject(url, object, object.getClass());
+		try {
+			String url = "http://localhost/" + subPath;
+			restTemplate.postForObject(url, object, object.getClass());
+		} catch (HttpClientErrorException e) {
+			json = new JSONObject(e.getResponseBodyAsString());
+			standardError = new StandardError(json.getInt("status"), json.getString("message"), json.getLong("timeStamp"));
+			addMessage(standardError);
+		}
 	}
 
 	public void update(T object, Long id, String subPath) {
@@ -37,5 +53,10 @@ public class GenericController<T> {
 	public void delete(Long id, String subPath) {
 		String url = "http://localhost/" + subPath + "/" + id;
 		restTemplate.delete(url, id);
+	}
+
+	public void addMessage(StandardError error) {
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, error.getMessage(), null);
+		FacesContext.getCurrentInstance().addMessage(null, message);
 	}
 }
